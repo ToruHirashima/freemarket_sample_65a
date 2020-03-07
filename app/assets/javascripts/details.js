c7eecd71c704fd3d4f650bf2daa56cf524771748
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-03-07 17:47:28 +0900

    Modify after review

--- app/assets/javascripts/details.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  console.log("load");
-  // 現時点では、該当ページで一度リロードしないとイベントリスナーが取り込まれないことに注意
-  $(".detailBoxPhoto__sub").on("click", ".detailBoxPhoto__tmb", function(e) {
-    let tmp = $(this).attr("src");
-    $(".detailBoxPhoto__main").attr("src", tmp);
-  });
-});
