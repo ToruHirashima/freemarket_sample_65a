d514494c39e3f93f608db11bef647f3cba2157f3
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-03-10 20:33:58 +0900

    Create orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, except: [:index]
+  resources :items, except: [:index] do
+    resources :orders  # アクションは後日絞り込み（newのみで良いと思われる）
+  end
   resources :users, only: [:index, :show]
 end
