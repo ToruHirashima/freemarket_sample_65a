e426fbb86aa9d6f7ea8c98ee1b14574570c0eae8
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-03-20 10:44:08 +0900

    Add reference key of items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_123121) do
+ActiveRecord::Schema.define(version: 2020_03_19_035055) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category", null: false
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 2020_03_16_123121) do
     t.integer "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.bigint "size_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["size_id"], name: "index_items_on_size_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -91,6 +95,8 @@ ActiveRecord::Schema.define(version: 2020_03_16_123121) do
   add_foreign_key "category_sizes", "sizes"
   add_foreign_key "deliveries", "users"
   add_foreign_key "images", "items"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "sizes"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
