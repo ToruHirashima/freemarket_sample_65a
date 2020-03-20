06fa7481fb08b5115279033db13f115d76c469a8
LIB-programming-beginner <ykykyk.worldout2015@gmail.com>
2020-03-20 10:30:42 +0900

    commit

--- db/migrate/20200301104315_create_deliveries.rb
+++ db/migrate/20200301104315_create_deliveries.rb
@@ -1,7 +1,6 @@
 class CreateDeliveries < ActiveRecord::Migration[5.2]
   def change
     create_table :deliveries do |t|
-      t.references :item, null: false, foreign_key: true
       t.integer :fee_burden, null: false
       t.integer :service, null: false
       t.string :area, null: false
