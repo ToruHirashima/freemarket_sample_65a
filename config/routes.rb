0c173acee220f9337f014a16ceb04ee1ab012a31
LIB-programming-beginner <ykykyk.worldout2015@gmail.com>
2020-03-21 16:51:20 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'items#index'
-  resources :items, except: [:index] do
+  resources :items do
     resources :orders  # only: [:new] としても良さそうですが一旦全アクションを実装
   end
   resources :users, only: [:index, :show]
