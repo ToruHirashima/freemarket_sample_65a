7a1283bb3e42a9d5fee9499a1f59bcf88426c35e
LIB-programming-beginner <ykykyk.worldout2015@gmail.com>
2020-03-21 14:12:12 +0900

    commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,4 +17,14 @@ class OrdersController < ApplicationController
   def set_image
     @image = Image.find(params[:item_id])
   end
+
+  def pay
+    Payjp.api_key = 'sk_test_b2c67dbefa1c1223a6878f7c'
+    Payjp::Charge.create(
+      #amountは値段を記載
+      amount: 3500, 
+      card: params['payjp-token'],
+      currency: 'jpy'
+    )
+  end
 end
