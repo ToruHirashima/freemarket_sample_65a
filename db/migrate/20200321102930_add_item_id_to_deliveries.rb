6ef555a55aeab194b61664d5068073fd1fafeab4
shibashiba48 <t1257476@gmail.com>
2020-03-29 21:47:46 +0900

    modify-migrate-file

--- db/migrate/20200321102930_add_item_id_to_deliveries.rb
+++ db/migrate/20200321102930_add_item_id_to_deliveries.rb
@@ -1,5 +1,4 @@
 class AddItemIdToDeliveries < ActiveRecord::Migration[5.2]
   def change
-    add_reference :deliveries, :item, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_102504) do
+ActiveRecord::Schema.define(version: 2020_03_21_102930) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category", null: false
