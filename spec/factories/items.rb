135af70d6b87e4b06a387a5abbac5f331ed25230
shibashiba48 <t1257476@gmail.com>
2020-04-12 08:40:01 +0900

    Update items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,13 @@ FactoryBot.define do
   factory :item do
     name        {Faker::Beer.brand}
     text        {Faker::Beer.name}
-    category    {"1"}
-    size        {"2"}
-    condition   {"新品、未使用"}
-    price       {"3000"}
-    status      {"0"}
+    category    
+    size        
+    condition   { 1 }
+    delivery
+    price       { 3000 }
+    user
+    status      { 0 }
     brand_name  {Faker::Beer.style}
   end
 end
