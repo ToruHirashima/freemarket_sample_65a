562b3fc9c594c7a72806fe2e5ae3f89b048bddb4
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-04-11 12:54:17 +0900

    Add test code 1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'can save' do
+      # テスト
+      it '1 + 1 が2になること' do
+        expect(1 + 1).to eq 2
+      end
+      # 商品名がなければ出品できないこと
+      it 'no name' do
+        item = Item.new(name: "",
+          text: "bbb",
+          condition: 1,
+          price: 900,
+          user_id: 1,
+          status: 0,
+          category_id: 1,
+          size_id: 1,
+          brand_name: nil)
+        item.valid?
+        # binding.pry
+        expect(item.errors[:name]).to include("can't be blank")
+      end
+    end
+  end
 end
