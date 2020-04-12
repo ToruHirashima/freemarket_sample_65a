78a25d35f216bcb00d6bc2e37bea72767dfc4468
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-04-12 23:17:16 +0900

    Modify category function 2

--- app/assets/javascripts/exh_categoris.js
+++ app/assets/javascripts/exh_categoris.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', ()=> {
   let cate_id = $('#exh-category').val();
   if (cate_id == "") { cate_id = 0 }
 
-  function buildChildren(data, idName, select_id) {
+  function buildHtml(data, idName, select_id) {
     let childHtml;
     data.forEach(function(child){
       let selectAttr = child.id == select_id ? `selected="selected" ` : '';
@@ -14,6 +14,16 @@ $(document).on('turbolinks:load', ()=> {
     return html;
   }
 
+  function selectData(data, depth) {
+    let select_data = [];
+    data.forEach(function(select){
+      if (select.depth == depth) {
+        select_data.push(select);
+      }
+    });
+    return select_data;
+  }
+
   $.ajax({
     url: '/items/category_parent',  // リクエストを送信するURLを指定
     type: "GET",  // HTTPメソッドを指定（デフォルトはGET）
@@ -23,51 +33,31 @@ $(document).on('turbolinks:load', ()=> {
     dataType: "json"  // レスポンスデータをjson形式と指定する
   })
   .done(function(data) {
-    let parent_id = 0
-    let children_id = 0
-    let grandchildren_id = 0
+    let parent_id = 0, children_id = 0, grandchildren_id = 0
     data.forEach(function(json){
       if (json.id == cate_id) {
         let id_len = json.path_ids.length;
-        parent_id = json.path_ids[0]
-        if (id_len > 1) { children_id = json.path_ids[1] }
-        if (id_len > 2) { grandchildren_id = json.path_ids[2] }
+        parent_id = json.path_ids[0];
+        if (id_len > 1) { children_id = json.path_ids[1]; }
+        if (id_len > 2) { grandchildren_id = json.path_ids[2]; }
       }
     });
-    
-    let parent_data = [];
-    data.forEach(function(parent){
-      if (parent.depth == 0) {
-        parent_data.push(parent);
-      }
-    });
-    $('#select_parent').append(buildChildren(parent_data, '', parent_id));
-    
+    $('#select_parent').append(buildHtml(selectData(data, 0), '', parent_id));
     if (parent_id != 0) {
-      let child_data = [];
-      data.forEach(function(child){
-        if (child.depth == 1) {
-          child_data.push(child);
-        }
-      });
-      $('#select_children').append(buildChildren(child_data, 'children_', children_id));
+      $('#select_children').append(buildHtml(selectData(data, 1), 'children_', children_id));
     }
-
     if (children_id != 0) {
-      let grandchild_data = [];
-      data.forEach(function(grandchild){
-        if (grandchild.depth == 2) {
-          grandchild_data.push(grandchild);
-        }
-      });
-      $('#select_grandchildren').append(buildChildren(grandchild_data, 'grandchildren_', grandchildren_id));
+      $('#select_grandchildren').append(buildHtml(selectData(data, 2), 'grandchildren_', grandchildren_id));
     }
   })
 
   $('#select_parent').on('change', '#item_category_id', function(e) {
     let inputNum = $(this).val();
     $('#exh-category').val(inputNum);
-    if (inputNum != "") {
+    if (inputNum == "") {
+      $("#select_children").empty();
+      $("#select_grandchildren").empty();
+    } else {
       $.ajax({
         url: '/items/category_children',  // リクエストを送信するURLを指定
         type: "GET",  // HTTPメソッドを指定（デフォルトはGET）
@@ -79,7 +69,7 @@ $(document).on('turbolinks:load', ()=> {
       .done(function(data) {
         $("#select_children").empty();
         $("#select_grandchildren").empty();
-        $('#select_children').append(buildChildren(data, 'children_', 0));
+        $('#select_children').append(buildHtml(data, 'children_', 0));
       })
     }
   }).change();
@@ -87,7 +77,9 @@ $(document).on('turbolinks:load', ()=> {
   $('#select_children').on('change', '#item_category_children_id', function(e) {
     let inputNum = $(this).val();
     $('#exh-category').val(inputNum);
-    if (inputNum != "") {
+    if (inputNum == "") {
+      $("#select_grandchildren").empty();
+    } else {
       $.ajax({
         url: '/items/category_grandchildren',  // リクエストを送信するURLを指定
         type: "GET",  // HTTPメソッドを指定（デフォルトはGET）
@@ -98,7 +90,7 @@ $(document).on('turbolinks:load', ()=> {
       })
       .done(function(data) {
         $("#select_grandchildren").empty();
-        $('#select_grandchildren').append(buildChildren(data, 'grandchildren_', 0));
+        $('#select_grandchildren').append(buildHtml(data, 'grandchildren_', 0));
       })
     }
   }).change();
