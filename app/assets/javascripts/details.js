a1e498749b0de4081aa3cb9706b2b47d958cc8f2
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-04-14 18:22:04 +0900

    Delete unnecessary files 2

--- app/assets/javascripts/details.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  console.log("load");
-  // 現時点では、該当ページで一度リロードしないとイベントリスナーが取り込まれないことに注意
-  $(".detail-box-photo__sub").on("click", ".detail-box-photo__tmb", function(e) {
-    let tmp = $(this).attr("src");
-    $(".detail-box-photo__main").attr("src", tmp);
-  });
-});
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,20 +2,20 @@ class OrdersController < ApplicationController
   # 商品購入確認ページ
   def new
     @item = Item.find(params[:item_id])
-    # binding.pry
   end
 
-  def create
-    @order = Order.new(item_id: params[:item_id], user_id: current_user.id)
-    if @order.save
-      # 失敗した場合にロールバックする処理が必要
-      @item= Item.find(params[:item_id])
-      @item.update(status: 1)
-      redirect_to root_path
-    else
-      render :new  # 不要（一応残しておく）
-    end
-  end
+  # 動作テスト用のため、createアクションはコメントアウト
+  # def create
+  #   @order = Order.new(item_id: params[:item_id], user_id: current_user.id)
+  #   if @order.save
+  #     # 失敗した場合にロールバックする処理が必要
+  #     @item= Item.find(params[:item_id])
+  #     @item.update(status: 1)
+  #     redirect_to root_path
+  #   else
+  #     render :new  # 不要（一応残しておく）
+  #   end
+  # end
 
   def show
     @order = Order.find(params[:id])
