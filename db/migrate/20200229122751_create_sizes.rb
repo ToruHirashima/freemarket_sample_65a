38a99b57b0dd76a7624fa2ddbe8f934fd9706041
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-04-16 12:36:46 +0900

    Delete migration files

--- db/migrate/20200229122751_create_sizes.rb
+++
@@ -1,9 +0,0 @@
-class CreateSizes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :sizes do |t|
-      t.string :kind_size, null:false
-      t.string :ancestry
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20200301103121_create_category_sizes.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategorySizes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :category_sizes do |t|
-      t.references :category, null: false, foreign_key: true
-      t.references :size, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
