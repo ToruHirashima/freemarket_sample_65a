a75473305e61bfca97468e2d8f16a5f01f7667c7
LIB-programming-beginner <ykykyk.worldout2015@gmail.com>
2020-04-17 20:50:39 +0900

    commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,25 @@
 class OrdersController < ApplicationController
   before_action :set_item, :set_image
   def new
+    @item = Item.find(params[:item_id])
+    @item.update(status: 2)
+    order = Order.new
+    order.user_id = 1
+    order.item_id = @item.id
+    order.save
+    redirect_to controller: 'orders', action: 'show', id: @item.id
   end
 
   def create
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
@@ -16,16 +29,17 @@ class OrdersController < ApplicationController
 
   def set_image
     @image = Image.find(params[:item_id])
-  end
+  end  
 
   require 'payjp'
 
   def purchase
-    Payjp.api_key = "sk_test_b2c67dbefa1c1223a6878f7c"
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     Payjp::Charge.create(
-      amount: 809, # 決済する値段
+      amount: @item.price,
       card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
       currency: 'jpy'
     )
+    redirect_to action: new
   end
 end
