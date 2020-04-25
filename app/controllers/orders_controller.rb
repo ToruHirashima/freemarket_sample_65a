9eea549f342a11f6f2331e5f6850892c6413eddd
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-04-26 00:08:49 +0900

    テスト用に購入機能の一部を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,17 +7,17 @@ class OrdersController < ApplicationController
   end
 
   # 動作テスト用のため、createアクションはコメントアウト
-  # def create
-  #   @order = Order.new(item_id: params[:item_id], user_id: current_user.id)
-  #   if @order.save
-  #     # 失敗した場合にロールバックする処理が必要
-  #     @item= Item.find(params[:item_id])
-  #     @item.update(status: 1)
-  #     redirect_to root_path
-  #   else
-  #     render :new  # 不要（一応残しておく）
-  #   end
-  # end
+  def create
+    @order = Order.new(item_id: params[:item_id], user_id: current_user.id)
+    if @order.save
+      # 失敗した場合にロールバックする処理が必要
+      @item= Item.find(params[:item_id])
+      @item.update(status: 1)
+      redirect_to root_path
+    else
+      render :new  # 不要（一応残しておく）
+    end
+  end
 
   def show
   end
